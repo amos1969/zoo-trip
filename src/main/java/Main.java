00f6617c75560517200a795804723bce0654f97d
Kit Ly <kit.ly@autotrader.co.uk>
2026-03-31 15:24:48 +0100

    Complete all zoo trip tasks.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList; // Import the ArrayList class
+
+
 public class Main {
+
+    static void main() {
+        ArrayList<String> bus = new ArrayList<String>();
+        bus.add("Ryan");
+        bus.add("Brandon");
+        bus.add("Rebecca");
+        bus.add("Richard");
+        bus.add("Jessica");
+        bus.add("Brian");
+        bus.add("Antoinne");
+        bus.add("Grace");
+        bus.add("Zee");
+        bus.add("Ibrahim");
+
+        //task 1
+        bus.remove("Imbrahim");
+        bus.add(0, "Imbrahim");
+
+        //task2
+        bus.remove("Rebecca");
+        bus.add("Rebecca");
+
+        System.out.println();
+
+        //task 3
+        bus.remove("Richard");
+        bus.add(bus.indexOf("Antoinne")-1, "Richard");
+
+        //task 4
+        bus.remove("Ryan");
+        System.out.println("Rebecca");
+        bus.add(bus.indexOf("Rebecca")-1, "Ryan");
+
+        System.out.println(bus);
+    }
+
+
+
+
 }
