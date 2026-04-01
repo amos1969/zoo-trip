ea9576305ed103c31541e245ee8fa41c26d0474c
Kit Ly <kit.ly@autotrader.co.uk>
2026-04-01 09:24:43 +0100

    Noticed error - spelling mistake when moving Ibrahim

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Main {
 
     static void main() {
-        ArrayList<String> bus = new ArrayList<String>();
+        ArrayList<String> bus = new ArrayList<>();
         bus.add("Ryan");
         bus.add("Brandon");
         bus.add("Rebecca");
@@ -16,8 +16,8 @@ public class Main {
         bus.add("Ibrahim");
 
         //task 1
-        bus.remove("Imbrahim");
-        bus.add(0, "Imbrahim");
+        bus.remove("Ibrahim");
+        bus.add(0, "Ibrahim");
         //task2
         bus.remove("Rebecca");
         bus.add("Rebecca");
